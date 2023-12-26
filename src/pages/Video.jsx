d5546d5bec5cce42a69db2fe00acc8cb949934ad
minhajul-im <minhajul.cpp@gmail.com>
2023-12-26 11:30:26 +0600

    �started our awesome project�

---
+++ src/pages/Video.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Video() {
+  return (
+    <div>
+      <h1>Hello Video page!</h1>
+    </div>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import FAQs from "./FAQs";
+import Home from "./Home";
+import Video from "./Video";
+import Reviews from "./Reviews";
+import Overview from "./Overview";
+import Newslater from "./Newslater";
+
+export { FAQs, Home, Video, Reviews, Overview, Newslater };
